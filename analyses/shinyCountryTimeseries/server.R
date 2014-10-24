7eb0981115ed45a9843c067e4e1c7e916caa82da
Mathew Hall <mathew.james.hall@gmail.com>
2014-10-24 16:49:08 +0100

    Show all countries in data set

--- analyses/shinyCountryTimeseries/server.R
+++ analyses/shinyCountryTimeseries/server.R
@@ -15,31 +15,28 @@ url <- "https://raw.githubusercontent.com/cmrivers/ebola/master/country_timeseri
 
 data <- getURL(url, ssl.verifypeer = FALSE)
 df <- read.csv(textConnection(data))
-
+#Drop the Date col
 df1_noDate <- df[, !names(df) %in% c("Date")]
-day <- c(0:df1_noDate[1, 1])
+#Build a series from 0...latest day in data set
+day <- c(0:max(df1_noDate$Day))
+#We'll add updates on each day we have data for each country here
 df3_merge <- data.frame(day)
-
-for(i in 2:ncol(df1_noDate)){
-  df_temp <- df1_noDate[, c(1, i)]
+#For each country:
+for(country in 2:ncol(df1_noDate)){
+  df_temp <- df1_noDate[, c(1, country)] #Day,(Cases|Deaths)_Country
+  #Data set is snapshots at day of reporting, with NAs representing "no change"/"no new data"
+  #so ignore those with NAs.
   df_temp <- na.omit(df_temp)
-  last <- nrow(df_temp)
-  last
-  df_temp[last,1]
-  day.offset <- df_temp[last,1]
-  df_temp$day.adj <- df_temp$Day - day.offset
-  df_temp
+
+  #Rescale all series so day 0 == first reported case/death
+  df_temp$day.adj <- df_temp$Day - min(df_temp$Day)
+
   df3_merge <- merge(x = df3_merge, y = df_temp[, names(df_temp) != "Day"],
                      by.x = "day", by.y = "day.adj", all.x = TRUE)
 }
 
 row.names(df3_merge) <- df3_merge$day
 df3_merge <- df3_merge[, names(df3_merge) != "day"]
-
-#df4 <- df3_merge %>%
-#    as.matrix() %>%
-#        t() %>%
-#            as.data.frame()
 
 df4 <- as.data.frame(t(as.matrix(df3_merge)))
 
@@ -52,23 +49,37 @@ names(df5_melt) <- c("type", "place", "day", "count")
 df5_melt$type[df5_melt$type == "Case"] <- "Cases"
 
 
+all <- unique(df5_melt$place)
+c_colors <- brewer.pal(length(all), 'Set1')
+names(c_colors) <- all
+
+theme_set(theme_minimal())
 
 shinyServer(function(input, output) {
 
   data_plot <- reactive({
     df_plot <- df5_melt[!is.na(df5_melt$count), ]
-    all <- c("Guinea", "Liberia", "SierraLeone", "Nigeria", "Senegal")
-    if(input$countries == "All"){selection <- all}
-    else{selection <- input$countries}
-    df_plot <- df_plot[df_plot$place %in% selection, ]
+	selection <- input$countries
+	if("All" %in% input$countries || length(input$countries) == 0 ){
+		selection <- all
+	}
+   df_plot %>% 
+	 filter(place %in% selection) %>%
+     mutate(count = as.numeric(count), day=as.numeric(day))
+  })
+  
+  output$countriesList <- renderUI({
+    checkboxGroupInput("countries",
+                       label = h3("Countries to display"),
+                       choices = c(all, "All"),
+                       selected = "All")
   })
 
+
+
   plot <- reactive({
-    all <- c("Guinea", "Liberia", "SierraLeone", "Nigeria", "Senegal")
-    c_colors <- brewer.pal(length(all), 'Set1')
-    names(c_colors) <- all
     g <- ggplot(data = data_plot(),
-                aes(x = as.numeric(day), y = as.numeric(count),
+                aes(x = day, y = count,
                     group = place, color = place)) +
                         geom_point() + geom_line()+
                             facet_grid(~ type) +
